f8c8783fbaf1b7d48b4d68d70792236b79175a8c
lsiddd <lucassidpacheco@gmail.com>
2020-03-04 18:36:22 -0300

    bashrc and install script update

--- post_install.sh
+++ post_install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-dnf update
+dnf update -y
 hostnamectl status # view current hostname
 hostnamectl set-hostname --static "koffee" # set up new hostname
 
-dnf update --refresh
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf update --refresh -y
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 systemctl enable sshd --now
 
-dnf install sysstat htop glances
+dnf install sysstat htop glances -y
 
 cd /tmp
 wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.9/Stacer-x86_64.AppImage
 chmod a+x stacer*.AppImage
 ./stacer*.AppImage
 
-dnf install youtube-dl vlc
+dnf install youtube-dl vlc -y
 
 dnf install \
 gstreamer-plugins-base \
@@ -30,12 +30,12 @@ gstreamer1-plugins-good \
 gstreamer1-plugins-good-extras \
 gstreamer1-plugins-bad-freeworld \
 ffmpeg \
-gstreamer-ffmpeg
+gstreamer-ffmpeg -y
 
-dnf install menulibre
+dnf install menulibre -y
 
 dnf install copyq -y
-dnf install gnome-tweak-tool
+dnf install gnome-tweak-tool -y
 
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x8320CA65CB2DE8E5"
 bash -c 'cat > /etc/yum.repos.d/onlyoffice.repo << 'EOF'
@@ -45,17 +45,17 @@ baseurl=http://download.onlyoffice.com/repo/centos/main/noarch/
 gpgcheck=1
 enabled=1
 EOF'
-dnf install onlyoffice-desktopeditors
+dnf install onlyoffice-desktopeditors -y
 
-dnf install unzip p7zip
+dnf install unzip p7zip -y
 
-dnf install flatpak
+dnf install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.spotify.Client
-flatpak install flathub org.telegram.desktop
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub org.telegram.desktop
 
-dnf install gnome-shell-extension-dash-to-dock.noarch
-dnf install gnome-shell-theme-flat-remix.noarch
-dnf install numix-icon-theme.noarch
-dnf install numix-gtk-theme.noarch
+dnf install gnome-shell-extension-dash-to-dock.noarch -y
+dnf install gnome-shell-theme-flat-remix.noarch -y
+dnf install numix-icon-theme.noarch -y
+dnf install numix-gtk-theme.noarch -y
 
